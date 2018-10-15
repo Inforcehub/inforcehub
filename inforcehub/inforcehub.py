f616000ba7b2018a3068dd967c8537eed73a4247
Matt Gosden <matt.gosden@inforcehub.com>
2018-10-15 16:03:55 +0100

    Added helloworld() function and initial test

---
+++ inforcehub/inforcehub.py
@@ -0,0 +1,10 @@
+
+
+def helloworld():
+    """
+    Function to confirm that the installation of this package was successful.
+    
+    :return: a message
+    """
+    return "Congratulations - inforcehub package now installed"
+    
--- test/test_inforcehub.py
+++ test/test_inforcehub.py
@@ -11,9 +11,10 @@ class TestInforcehub(object):
     def setup_class(cls):
         pass
 
-    def test_something(self):
-        pass
-
     @classmethod
     def teardown_class(cls):
         pass
+
+    def test_helloworld(self):
+        """ Tests the helloworld method used to check installation was successful """
+        assert "installed" in inforcehub.helloworld()
