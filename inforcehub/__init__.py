d9700ac6c1ffa69e1e6c9873ddd636e3638c17f5
Matt Gosden <matt.gosden@inforcehub.com>
2018-10-15 16:42:28 +0100

    Updated init to load helloworld module

--- inforcehub/__init__.py
+++ inforcehub/__init__.py
@@ -1,3 +1,5 @@
-__author__ = 'Matt Gosden'
-__email__ = 'mdgosden@gmail.com'
-__version__ = '0.1.0'
+__author__ = "Inforcehub"
+__email__ = "matt.gosden@inforcehub.com"
+__version__ = "0.1.0"
+
+from .inforcehub import helloworld
