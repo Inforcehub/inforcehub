4c58ed220a1656b537944bb6adb607ceb87e3381
Matt Gosden <matt.gosden@inforcehub.com>
2018-10-15 17:05:35 +0100

    Removed Travis python 3.7 and 3.8 builds

--- setup.py
+++ setup.py
@@ -44,8 +44,6 @@ setup(
         "Programming Language :: Python :: 3 :: Only",
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
         "Operating System :: OS Independent",
         "Topic :: Office/Business :: Financial",
         "Topic :: Software Development :: Libraries :: Python Modules",
