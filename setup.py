86956839b29ba106ebd140d151b48c4b647120e4
Matt Gosden <matt.gosden@inforcehub.com>
2018-10-16 11:40:06 +0100

    tweak to fix error on setup.py

--- setup.py
+++ setup.py
@@ -41,7 +41,6 @@ setup(
         "Intended Audience :: Financial and Insurance Industry",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Natural Language :: English",
-        "Programming Language :: Python :: pypy",
         "Programming Language :: Python :: 3 :: Only",
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
